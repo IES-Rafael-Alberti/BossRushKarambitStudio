d22f92c93f305e3dbc51be070a9dcfca00bd26e0
MVPGamesOF <valverde.bemar20@cadiz.salesianos.edu>
2025-01-10 12:50:09 +0100

    Adicion de los scripts del player y del enemigo

--- Los Giros/Assets/Scripts/Controllers/TurnController.cs
+++ Los Giros/Assets/Scripts/Controllers/TurnController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 
-public class ControladorPartida : MonoBehaviour
+public class TurnController : MonoBehaviour
 {
     private int cantidadCartasBaraja, tiempoLimite = 0;
     public static int contResultado = 0;

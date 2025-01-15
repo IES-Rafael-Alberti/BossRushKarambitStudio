335dd0916fd5cd793dd859cf8fb1dc5cc0f365fe
Ancarix <ancarix96@gmail.com>
2025-01-15 09:56:31 +0100

    Save 2 15-01-25

--- Los Giros/Assets/Scripts/Controllers/SeleccionCarta.cs
+++ Los Giros/Assets/Scripts/Controllers/SeleccionCarta.cs
@@ -23,6 +23,7 @@ public class SeleccionCarta : MonoBehaviour, IPointerClickHandler, IPointerEnter
     public void OnPointerClick(PointerEventData eventData)
     {
         //Hacer click a un objeto
+        Debug.Log("Click.");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -32,7 +33,7 @@ public class SeleccionCarta : MonoBehaviour, IPointerClickHandler, IPointerEnter
         //{
         //    eventData.selectedObject = gameObject;
         //}
-        Debug.Log("Click.");
+        Debug.Log("Carta seleccionada.");
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -42,7 +43,7 @@ public class SeleccionCarta : MonoBehaviour, IPointerClickHandler, IPointerEnter
         //{
         //    eventData.selectedObject = null;
         //}
-        Debug.Log("Clack.");
+        Debug.Log("Carta deseleccionada.");
 
     }
 

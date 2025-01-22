b604544cca851a7e0822504a3a333dc8c30b226d
Ancarix <ancarix96@gmail.com>
2025-01-22 10:41:26 +0100

    Pause Menu WIP
    Creado el menu de pausa
    
    Falta la posibilidad de activarse con un input.

---
+++ Los Giros/Assets/Scripts/Controllers/PauseController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+
+    [SerializeField] GameObject pauseMenuCanvas;
+
+    public void PauseButton()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Pause();
+
+        }
+        
+    }
+
+    public void Pause()
+    {
+        pauseMenuCanvas.SetActive(true);
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}

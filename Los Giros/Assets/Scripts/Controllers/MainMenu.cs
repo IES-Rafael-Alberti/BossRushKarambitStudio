115e0e6dafe9a9241b0030be51fbbc250b5a645b
Ancarix <ancarix96@gmail.com>
2025-01-31 11:43:12 +0100

    Fusion de los MainMenu (MainMenuFinal)

--- Los Giros/Assets/Scripts/Controllers/MainMenu.cs
+++ Los Giros/Assets/Scripts/Controllers/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuA : MonoBehaviour
 {
     public static MainMenu instance;
     [SerializeField] Animator transitionAnim;
